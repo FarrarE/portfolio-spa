f426205bd334d036e61e924f893bd63ae163152f
urza <efarrar@pdx.edu>
2020-01-14 00:10:27 -0800

    Added repo and deployment links.

--- src/containers/Projects/index.js
+++ src/containers/Projects/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {
+import { CardLink, Badge,
   UncontrolledPopover, PopoverHeader, PopoverBody,
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button, CardHeader, CardFooter
@@ -14,8 +14,12 @@ function Projects() {
         <CardBody>
           <CardTitle>DMScreen</CardTitle>
           <CardText>DMScreen is a web app tool for Dungeons Masters to track important information about the game they are playing. This version has been designed specifically for Dungeon and Dragons 5th Edition and is open source to allow others to adapt it for other editions.</CardText>
-          <Button>Repo</Button>
-          <Button>Deployment</Button>
+          <CardLink href="https://github.com/FarrarE/DMScreen?" target="_blank">
+            <Button>Repo</Button>
+          </CardLink>
+          <CardLink href="https://master.d1w4ohx8frzj9u.amplifyapp.com/?" target="_blank">
+            <Button>Deployment</Button>
+          </CardLink>
         </CardBody>
         <CardFooter className="card-footer">
           <i id="reactPopover" class="devicon-react-original colored"></i>
@@ -28,8 +32,12 @@ function Projects() {
           <CardTitle>MTGSpellbook</CardTitle>
           <CardText>MTGSpellbook is a web app that allows users to store information about their Magic the Gathering collection. Using AWS API gateways, it can store user information in DynamoDB and uses Cognito identity pools to authenticate user login and API requests.
           </CardText>
-          <Button>Repo</Button>
-          <Button>Deployment</Button>
+          <CardLink href="https://github.com/FarrarE/mtgspellbook?" target="_blank">
+            <Button>Repo</Button>
+          </CardLink>
+          <CardLink href="https://prod.dv0y2bh0z5aj9.amplifyapp.com/?" target="_blank">
+            <Button>Deployment</Button>
+          </CardLink>
         </CardBody>
         <CardFooter className="card-footer">
           <i id="reactPopover" class="devicon-react-original colored"></i>
@@ -42,8 +50,12 @@ function Projects() {
         <CardBody>
           <CardTitle>5E Character Sheet</CardTitle>
           <CardText>A digital charactersheet for Dungeons and Dragons 5th Edition that stores the information in your local browser for later use.</CardText>
-          <Button>Repo</Button>
-          <Button>Deployment</Button>
+          <CardLink href="https://github.com/FarrarE/5eCharacterSheet?" target="_blank">
+            <Button>Repo</Button>
+          </CardLink>
+          <CardLink href="https://farrare.github.io/5eCharacterSheet/?" target="_blank">
+            <Button>Deployment</Button>
+          </CardLink>
         </CardBody>
         <CardFooter className="card-footer">
           <i id="bootstrapPopover" class="devicon-bootstrap-plain colored"></i>
