51edfdb8b4b111dc8482f6705c76bc69939c40e1
urza <efarrar@pdx.edu>
2020-01-14 19:18:53 -0800

    Changed to react router links.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem} from 'reactstrap';
+import { Link } from "react-router-dom";
 import { GitHub } from 'react-feather';
 import { Linkedin } from 'react-feather';
 import { Mail } from 'react-feather';
@@ -10,21 +11,21 @@ function Header() {
     <div className="navbar">  
       <Nav >
           <NavItem>
-          <NavLink href="/about">About</NavLink>
+            <Link to="/about" className="nav-link">About</Link>
           </NavItem>
           <NavItem>
-          <NavLink href="/projects">Project</NavLink>
+            <Link to="/projects" className="nav-link">Project</Link>
           </NavItem>
       </Nav>
       <Nav className="social">
         <NavItem>
-            <NavLink href="https://github.com/FarrarE" target="_blank"><GitHub /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink href="https://www.linkedin.com/in/ezra-farrar-149259173/" target="_blank"><Linkedin /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink href="mailto: contact.efarrar@gmail.com" target="_blank"><Mail /></NavLink>
+          <Link to="https://github.com/FarrarE" target="_blank" className="nav-link"><GitHub /></Link>
+          </NavItem>
+          <NavItem>
+          <Link to="https://www.linkedin.com/in/ezra-farrar-149259173/" target="_blank" className="nav-link"><Linkedin /></Link>
+          </NavItem>
+          <NavItem>
+          <Link to="mailto: contact.efarrar@gmail.com" target="_blank" className="nav-link"><Mail /></Link>
         </NavItem>
       </Nav>
     </div>
