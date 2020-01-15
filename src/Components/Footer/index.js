0703c4613dc5d1449fe0f23e4075fc1e81df67f5
urza <efarrar@pdx.edu>
2020-01-14 18:52:18 -0800

    Finished basic footer.

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,11 +4,15 @@ import "./styles.css";
 function Footer() {
   return (
     <div className="Footer"> 
-        <div className="signature">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a>
+        <div >Icons made by 
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank"> Freepik </a> from 
+            <a href="https://www.flaticon.com/" title="Flaticon" target="_blank"> www.flaticon.com</a>. Other Icons from 
+            <a href="https://feathericons.com/" title="Feather Icons" target="_blank"> Feather Icons</a> and
+            <a href="https://konpa.github.io/devicon/" title="Devicon" target="_blank"> Devicon</a>.
         </div>
-        <a href="https://feathericons.com/" title="Feather Icons" target="_blank">Feather Icons</a> 
-        <a href="https://konpa.github.io/devicon/" title="Devicon" target="_blank">Devicon</a> 
-        © Copyright 2020 Ezra Farrar 
+        <div className="copyright">
+        &nbsp; © Copyright 2020 Ezra Farrar 
+        </div>
     </div>
   );
 }
