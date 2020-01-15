f251fd2ebaef2b499c6c0cbbcd0392412c189092
urza <efarrar@pdx.edu>
2020-01-14 17:20:22 -0800

    Basic layout in place.

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Fade, Jumbotron } from 'reactstrap';
+import { Fade} from 'reactstrap';
 import "./styles.css";
 import cooking from "./icons/cutting-board.svg";
 import code from "./icons/code.svg";
@@ -10,8 +10,7 @@ import games from "./icons/board-games.svg";
 function About() {
   return (
     <Fade className="About">
-        <Jumbotron>
-        <h1 className="display-3">Ezra Farrar</h1>
+        <h2 className="display-3">Ezra Farrar</h2>
         <p className="lead">Web Developer</p>
         <hr className="my-2" />
         <p>I'm a Portland State University graduate with a Bachelor's in Computer Science.</p>
@@ -26,9 +25,8 @@ function About() {
           <object className="hobby" type="image/svg+xml" data={art} />
           <object className="hobby" type="image/svg+xml" data={games} />
           </div>
-          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+          <div className="signature">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
         </div>
-      </Jumbotron>
     </Fade>
   );
 }
